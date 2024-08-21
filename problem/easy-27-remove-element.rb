748c95b4a2b8ceae54220af33af2877562ea38d2
saeyama <camper_green_yellow_21@yahoo.co.jp>
2024-08-21 16:57:20 +0900

    add 27.remove-element

---
+++ problem/easy-27-remove-element.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=27 lang=ruby
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+def remove_element(nums, val)
+  nums.delete(val)
+  nums.size
+end
+# @lc code=end
