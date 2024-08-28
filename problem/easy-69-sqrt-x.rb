5c6d91a65db3082aade049ad414ffd44b67f8f8e
saeyama <camper_green_yellow_21@yahoo.co.jp>
2024-08-28 19:55:21 +0900

    add newton 69.sqrt-x

--- problem/easy-69-sqrt-x.rb
+++ problem/easy-69-sqrt-x.rb
@@ -11,19 +11,28 @@ def my_sqrt(x)
   # Math.sqrt(x).truncate
   # Math.sqrt(x).to_i
   # (x ** 0.5).to_i
-  left = 0
-  right = x
-  while left <= right
-    mid = (left + right) / 2
 
-    if mid * mid > x
-      right = mid - 1
-    elsif mid * mid < x
-      left = mid + 1
-    else
-      return mid
-    end
-  end
-  right
+  # binary search
+  # left = 0
+  # right = x
+  # while left <= right
+  #   mid = (left + right) / 2
+
+  #   if mid * mid > x
+  #     right = mid - 1
+  #   elsif mid * mid < x
+  #     left = mid + 1
+  #   else
+  #     return mid
+  #   end
+  # end
+  # right
+
+  # newton
+  return x if x < 2
+
+  r = x
+  r = (r + x / r) / 2 while r * r > x
+  r
 end
 # @lc code=end
