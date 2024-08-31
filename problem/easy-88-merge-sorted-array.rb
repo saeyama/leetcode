615312c8e69d9ffa4a93e7283a24fc4d64e2ebdc
saeyama <camper_green_yellow_21@yahoo.co.jp>
2024-08-31 15:09:06 +0900

    add 88.merge-sorted-array

---
+++ problem/easy-88-merge-sorted-array.rb
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=88 lang=ruby
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+  nums1.slice!(m..-1)
+  nums1 << nums2
+  nums1.flatten!.sort!
+end
+# @lc code=end
