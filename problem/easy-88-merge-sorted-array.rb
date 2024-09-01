afc76229c02abdfb1dcaa0b096315527738b5182
saeyama <camper_green_yellow_21@yahoo.co.jp>
2024-09-01 21:08:25 +0900

    add refactoring 88.merge-sorted-array

--- problem/easy-88-merge-sorted-array.rb
+++ problem/easy-88-merge-sorted-array.rb
@@ -11,8 +11,10 @@
 # @param {Integer} n
 # @return {Void} Do not return anything, modify nums1 in-place instead.
 def merge(nums1, m, nums2, n)
-  nums1.slice!(m..-1)
-  nums1 << nums2
-  nums1.flatten!.sort!
+  # nums1.slice!(m..-1)
+  # nums1 << nums2
+  # nums1.flatten!.sort!
+  nums1[m, n] = nums2
+  nums1.sort!
 end
 # @lc code=end
