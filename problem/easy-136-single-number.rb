d25c7b3aaa125678e58f1fba13a7e40813c6dfb0
saeyama <camper_green_yellow_21@yahoo.co.jp>
2024-09-03 21:49:15 +0900

    add 136.single-number

---
+++ problem/easy-136-single-number.rb
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=136 lang=ruby
+#
+# [136] Single Number
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer}
+def single_number(nums)
+  nums.tally.invert[1]
+end
+# @lc code=end
