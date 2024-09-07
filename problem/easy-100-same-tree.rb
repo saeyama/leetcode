ff2a6332944ec9881ed0647bf151391fbe51dab3
saeyama <camper_green_yellow_21@yahoo.co.jp>
2024-09-08 00:00:46 +0900

    add refactoring 100-same-tree

--- problem/easy-100-same-tree.rb
+++ problem/easy-100-same-tree.rb
@@ -6,29 +6,38 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-class TreeNode
-  attr_accessor :val, :left, :right
+# class TreeNode
+#   attr_accessor :val, :left, :right
 
-  def initialize(val = 0, left = nil, right = nil)
-    @val = val
-    @left = left
-    @right = right
-  end
-end
+#   def initialize(val = 0, left = nil, right = nil)
+#     @val = val
+#     @left = left
+#     @right = right
+#   end
+# end
 # @param {TreeNode} p
 # @param {TreeNode} q
 # @return {Boolean}
 def is_same_tree(p, q)
+  return true if p.nil? && q.nil?
   return false if p.nil? || q.nil?
   return false if p.val != q.val
 
-  if p.left && q.left && p.left.val == q.left.val
-    is_same_tree(p.left, q.left)
-  elsif p.left && q.left && p.right.val == q.right.val
-    is_same_tree(p.right, q.right)
-  end
+  left_same = is_same_tree(p.left, q.left)
+  right_same = is_same_tree(p.right, q.right)
 
-  true
+  left_same && right_same
+
+  # return false if p.nil? || q.nil?
+  # return false if p.val != q.val
+
+  # if p.left && q.left && p.left.val == q.left.val
+  #   is_same_tree(p.left, q.left)
+  # elsif p.left && q.left && p.right.val == q.right.val
+  #   is_same_tree(p.right, q.right)
+  # end
+
+  # true
 end
 # @lc code=end
 
